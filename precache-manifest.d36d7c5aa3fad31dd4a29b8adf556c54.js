ddd0f4d084bd19b70fa8bab6b01755e66e9bf9f5
ZenBunZakuro <zenbunzakuro@gmail.com>
2019-04-16 14:14:52 +0300

    Updates

---
+++ precache-manifest.d36d7c5aa3fad31dd4a29b8adf556c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "54353f63fc2582fbca37",
+    "url": "/hello/static/js/main.eb3d8feb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/hello/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "54353f63fc2582fbca37",
+    "url": "/hello/static/css/main.4d5b35bd.chunk.css"
+  },
+  {
+    "revision": "1d00775b659d975716324572ed451810",
+    "url": "/hello/index.html"
+  }
+];
